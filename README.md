8484be48912a4f9df4cf98b13219fd42baf7e8ca
Davide Melfi <darklight3it@gmail.com>
2025-09-07 13:19:27 +0100

    chore: update .gitignore, Cargo.toml, and README; add download_model.sh script

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # llmender
 A rust cli application to fix ebook metadata with LLMs
 
+## Todo
+
+- [x] Script to download and update llama model.
+- [ ] Try playing with it.
+- [ ] Test with a mock up json model.
+- [ ] Unit Testing Strategy
+- [ ] Check why it is not able to load the model.
+- [ ] Research how to package models in single applications
+
+
+## Ideas
+
+- [ ] hooks on a folder. automatic librarian
+- [ ] Change the name to ai librarian. 
+
 
 ```
+file system example
+
 llmender/
 ├─ Cargo.toml
 ├─ Cargo.lock
---
+++ download_model.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+set -e
+
+# ---------------------------
+# Configurations
+# ---------------------------
+MODEL_NAME="openbmb/MiniCPM-Llama3-V-2_5-gguf"
+MODEL_VERSION="ggml-model-IQ4_XS.gguf"
+MODEL_DIR="./models"
+TEMP_DIR="./tmp_models"
+
+# ---------------------------
+# Clean folders
+# ---------------------------
+
+rm -rf "$MODEL_DIR" && rm -rf "$TEMP_DIR"
+
+# ---------------------------
+# Create folders
+# ---------------------------
+mkdir -p "$MODEL_DIR" && mkdir -p "$TEMP_DIR"
+
+# ---------------------------
+# Download the model from Hugging Face
+# ---------------------------
+echo "📥 Downloading model from Hugging Face..."
+
+
+hf download "$MODEL_NAME" \
+    --local-dir "$TEMP_DIR" \
+    --include "$MODEL_VERSION"
+
+# if there is the --convert-to-gguf flag, we convert the model
+if [ "$1" == "--convert-to-gguf" ]; then
+    echo "🔄 Converting model to GGUF..."
+
+    # ---------------------------
+    # Clone llama.cpp if necessary
+    # ---------------------------
+    if [ ! -d "$TEMP_DIR/llama.cpp" ]; then
+        echo "🦙 Cloning llama.cpp..."
+        git clone
+    fi
+
+    # ---------------------------
+    # Install Python requirements
+    # ---------------------------
+    pip install -r "$TEMP_DIR/llama.cpp/requirements.txt"
+
+    # ---------------------------
+    # Convert the model to GGUF
+    # ---------------------------
+    echo "🔄 Converting model to GGUF..."
+
+    python3 "$TEMP_DIR/llama.cpp/convert_hf_to_gguf.py" "$TEMP_DIR" --outfile "$MODEL_DIR/llama3-8B.gguf" --outtype q8_0
+fi
+
+# ---------------------------
+# Cleanup temporary files
+# ---------------------------
+echo "🧹 Cleaning up temporary files..."
+mv $TEMP_DIR/* $MODEL_DIR
+rm -rf "$TEMP_DIR"
+
+echo "✅ Model ready at ${MODEL_DIR}"
