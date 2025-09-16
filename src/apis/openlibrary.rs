87289bd21712489fe74e63eddb6aec1b3fc87f66
Davide Melfi <darklight3it@gmail.com>
2025-09-16 19:32:30 +0200

    chore: implement openlibrary

---
+++ src/apis/openlibrary.rs
@@ -0,0 +1,47 @@
+use crate::apis::models::OpenLibraryResponse;
+
+use super::errors::ClientError;
+use super::models::Book;
+use log::info;
+
+pub async fn fetch_book_by_isbn(isbn: &String) -> Result<Book, ClientError> {
+    // Example: GET request to Open Library API
+    let url = format!("https://openlibrary.org/search.json?isbn={}", isbn);
+
+    // ? operator is returning the response and bubbling the error to the caller
+    // internalerror is automatically deriving the Error from ClientError enum
+    let response = reqwest::get(&url).await?;
+    let body = response.text().await?;
+
+    info!("Raw:{}", body);
+
+    let response: OpenLibraryResponse = serde_json::from_str(&body)?;
+
+    // Openlibrary Search API return 200 even if the result bears 0 books
+    // in this case we are searching for ISBN so either we get 1 result or nothing
+    if let Some(doc) = response.docs.into_iter().next() {
+        Ok(doc)
+    } else {
+        Err(ClientError::Api {
+            status: reqwest::StatusCode::NOT_FOUND,
+            message: "Book not found".to_string(),
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tokio;
+
+    #[tokio::test]
+    async fn test_fetch_book_by_isbn() {
+        let isbn = &"9780141439600".to_string(); // Pride and Prejudice
+        let book: Book = fetch_book_by_isbn(isbn).await.unwrap();
+
+        assert_eq!(book.title, "Pride and Prejudice");
+        let authors = book.authors;
+        assert!(!authors.is_empty());
+        assert_eq!("Jane Austen", authors.iter().next().unwrap().key);
+    }
+}
